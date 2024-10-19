4f9224101844cf57fc03e991c08312cadfce2af5
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-19 23:16:46 +0300

    .

--- lab-1/ex4.js
+++ lab-1/ex4.js
@@ -1,15 +1,18 @@
 function arr(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-
-            if (i > 1 && j > 1 && i < arr.length && j < arr[i].length) {
-                sum += arr[i][j];
-                console.log(sum);
-
-            }
-
-        }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (i > 1 && j > 1 && i < arr.length && j < arr[i].length) {
+        sum += arr[i][j];
+        console.log(sum);
+      }
     }
+  }
 }
-arr([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+//to-do: fix the exercise
+arr([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
