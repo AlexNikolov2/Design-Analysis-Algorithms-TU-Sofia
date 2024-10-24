b41bc25ee19e2191b04e86f86669280115ff8512
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-24 23:57:22 +0300

    .

--- lab-1/ex3.js
+++ lab-1/ex3.js
@@ -1,31 +1,30 @@
 function arr(arr) {
-    //find the number of the equal elements in the longest round of ones
+  //find the number of the equal elements in the longest round of ones
 
-    let round = 1;
-    let currentround = 1;
-    let equals = 0;
+  let round = 1;
+  let currentround = 1;
+  let equals = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] === arr[i + 1]) {
-            currentround++;
-            equals++
-        } else {
-            if (currentround > round) {
-                round = currentround;
-                equals++;
-            }
-            currentround = 1;
-        }
-
-    }
-
-    if (currentround > round) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      currentround++;
+      equals++;
+    } else {
+      if (currentround > round) {
         round = currentround;
+        equals++;
+      }
+      currentround = 1;
     }
+  }
 
-    console.log(round);
-    console.log(equals);
+  if (currentround > round) {
+    round = currentround;
+  }
 
+  console.log(round);
+  console.log(equals);
 }
-arr([1, 2, 3, 4, 5, 5, 5, 6, , 7, 7, 8, 8, 8, 8])
+
+//fix
+arr([1, 2, 3, 4, 5, 5, 5, 6, , 7, 7, 8, 8, 8, 8]);
