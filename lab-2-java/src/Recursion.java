cb23cfaed8a86d2b275ffc7931921a48661c6d29
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-15 11:51:34 +0200

    old js-er can always do it

--- lab-2-java/src/Recursion.java
+++ lab-2-java/src/Recursion.java
@@ -1,3 +1,14 @@
 public class Recursion {
-    
+    public static void main(String[] args) {
+        int number = 10;
+        String binary = DecToBinRecursive(number);
+        System.out.println("Binary representation of " + number + ": " + binary);
+    }
+
+    public static String DecToBinRecursive(int num) {
+        if (num == 0) {
+            return ""; 
+        }
+        return DecToBinRecursive(num / 2) + (num % 2); 
+    }
 }
---
+++ lab-2/palindrome.js
@@ -0,0 +1,21 @@
+function Palindrome() {
+    let string = 'level';
+    let isPalindrome = false;
+    let reverse = [];
+
+    for (let i = string.length - 1; i >= 0; i--) {
+
+        reverse.push(string.charAt(i))
+
+    }
+    reverse = reverse.join('');
+
+    if (string === reverse) {
+        isPalindrome = true;
+    }
+
+
+    console.log(isPalindrome);
+
+}
+Palindrome()
