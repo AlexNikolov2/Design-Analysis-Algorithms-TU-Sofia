9b6becf8293b20cb3663f0d971de3d14ed6ab68d
Aleks Nikolov <tcr.official.world@gmail.com>
2025-01-22 22:15:41 +0200

    pleshiv, leke, jena ti se ebe

--- udemy-course/ex1.c
+++ udemy-course/ex1.c
@@ -1,3 +1,12 @@
-void main(){
-    printf("Welcome!");
+void heronFormula(int a, int b, int c){
+    int half_perimeter = (a + b + c) / 2;
+
+    float size = (float) (sqrt(half_perimeter * (half_perimeter - a) * (half_perimeter - b) * (half_perimeter - c)));
+
+    printf("%.2f \n", size);
+}
+
+int main(){
+    heronFormula(3,4,5);
+    return 0;
 }
